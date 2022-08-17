6f29c036a62b50d78ff7606520870785f7a52f75
Wahib Kerkeni <mr.wahib@gmail.com>
2022-08-17 12:47:56 +0100

    Initial Project Add

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# spring5-mvc-rest
+![Spring](https://img.shields.io/badge/spring-%236DB33F.svg?style=for-the-badge&logo=spring&logoColor=white)
+
+# Spring Framework 5 MVC Rest Application
 Restful Interfaces with Spring 5
---
+++ src/test/java/mrw007/springframework/spring5mvcrest/Spring5MvcRestApplicationTests.java
@@ -0,0 +1,13 @@
+package mrw007.springframework.spring5mvcrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5MvcRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
