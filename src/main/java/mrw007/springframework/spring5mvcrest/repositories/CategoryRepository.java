0b39350f0e9d289532dd0f4f31e9b41238887e94
Wahib Kerkeni <mr.wahib@gmail.com>
2022-08-17 13:03:38 +0100

    Added Category Model

---
+++ src/main/java/mrw007/springframework/spring5mvcrest/repositories/CategoryRepository.java
@@ -0,0 +1,7 @@
+package mrw007.springframework.spring5mvcrest.repositories;
+
+import mrw007.springframework.spring5mvcrest.models.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
