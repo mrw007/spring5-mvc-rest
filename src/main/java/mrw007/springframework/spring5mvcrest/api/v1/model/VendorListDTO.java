3cfba06b0c7b30e76db00dbe0d12ade09b69d9f4
Wahib Kerkeni <mr.wahib@gmail.com>
2022-08-20 21:54:02 +0100

    Added Vendor List DTO

---
+++ src/main/java/mrw007/springframework/spring5mvcrest/api/v1/model/VendorListDTO.java
@@ -0,0 +1,14 @@
+package mrw007.springframework.spring5mvcrest.api.v1.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class VendorListDTO {
+    private List<VendorDTO> vendors;
+}
